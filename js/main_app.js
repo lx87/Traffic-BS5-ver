e4d5dbd47211e73b3cb43c0eab582dad3bec8848
lx87 <limarx87@gmail.com>
2024-09-23 18:00:06 +0300

    Fixed path

--- js/main_app.js
+++ js/main_app.js
@@ -1,3 +1,29 @@
+//ACCORDION
+
+const elements = document.querySelectorAll('.accordion_element');
+
+elements.forEach(element => {
+  element.addEventListener('click', () => { 
+
+    const content = element.querySelector('.accordion_content');
+    const icon = element.querySelector('.accordion_icon');
+
+    content.classList.toggle('active');
+    element.classList.toggle('active');
+
+    if (content.classList.contains('active')) {
+      content.style.maxHeight = content.scrollHeight + 'px';
+      icon.classList.add('active');
+      
+    } else {
+      content.style.maxHeight = 0;
+      icon.classList.remove('active');
+    }
+  });
+});
+
+
+
 //MENU
 
 const openMenuBtn = document.getElementById('open_menu');
@@ -25,29 +51,6 @@ flatpickr("#calltime", {
     time_24hr: false  // Для 24-часового формата
   });
 
-//ACCORDION
-
-const elements = document.querySelectorAll('.accordion_element');
-
-elements.forEach(element => {
-  element.addEventListener('click', () => { 
-
-    const content = element.querySelector('.accordion_content');
-    const icon = element.querySelector('.accordion_icon');
-
-    content.classList.toggle('active');
-    element.classList.toggle('active');
-
-    if (content.classList.contains('active')) {
-      content.style.maxHeight = content.scrollHeight + 'px';
-      icon.classList.add('active');
-      
-    } else {
-      content.style.maxHeight = 0;
-      icon.classList.remove('active');
-    }
-  });
-});
 
 
 //FORM VALIDATION
